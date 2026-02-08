06f0261fcea209612b990f609804f702466a8432
BryanEstrada003 <mbestrad@espol.edu.ec>
2026-02-08 00:11:18 -0500

    feat: init Talk section

--- src/lib/data/nav-bar.ts
+++ src/lib/data/nav-bar.ts
@@ -9,7 +9,7 @@ const items: Array<{
 	{ title: 'Projects', icon: 'i-carbon-cube', href: '/projects' },
 	{ title: 'Experience', icon: 'i-carbon-development', href: '/experience' },
 	{ title: 'Education', icon: 'i-carbon-education', href: '/education' },
-	{ title: 'Talks', icon: 'i-carbon-user-speaker', href: '/talks' },
+	// { title: 'Talks', icon: 'i-carbon-user-speaker', href: '/talks' },
 	{ title: 'Resume', icon: 'i-carbon-document', href: '/resume' }
 ];
 
